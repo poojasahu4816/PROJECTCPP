ada04a02eb1ad5c7c85a56e62ed6fb298784b640
poojasahu4816 <misspooja.4816@gmail.com>
2025-11-21 06:44:07 +0530

    swap1

---
+++ Coder_Army.cpp/lec11switch.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    cout<<"Enter a number: ";
+    cin >> i;
+
+    switch (i)
+    {
+        case 1:
+        cout<<"rohit";
+        break;
+
+        case 2:
+        cout<<"mohit";
+        break;
+
+        case 3:
+        cout<<"sohit";
+        break;
+
+        case 4:
+        cout<<"johit";
+        break;
+
+        default:
+        cout <<"rahul";
+
+    }
+}
