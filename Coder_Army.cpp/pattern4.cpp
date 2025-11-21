deaa23c85e8b9765fd7388192eaf92ced8637e69
poojasahu4816 <misspooja.4816@gmail.com>
2025-11-21 06:36:51 +0530

    pattern3

---
+++ Coder_Army.cpp/pattern4.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i,n,j;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    for(j=1 ; j<=n ; j++)
+    {
+        for(i=1 ; i<=j ; i++){
+        cout <<" *";
+        }
+        {
+        for(i=1 ; i<=2*n-2*j ; i++)
+        cout <<"  ";
+        }
+        {
+        for(i=1 ; i<=j ; i++)
+        cout <<" *";
+       }
+    cout <<endl;
+}
+for(j=n-1 ; j>=1 ; j--)
+    {
+        for(i=1 ; i<=j ; i++){
+        cout <<" *";
+        }
+        {
+        for(i=1 ; i<=2*n-2*j ; i++)
+        cout <<"  ";
+        }
+        {
+        for(i=1 ; i<=j ; i++)
+        cout << " *";
+       }
+    cout <<endl;
+}
+}
