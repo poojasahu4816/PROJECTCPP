a2aef822c292e34f7389b0f889a9eb53c0318b4a
Pooja Sahu <misspooja.4816@gmail.com>
2025-12-25 16:01:12 +0530

    sheet -> 24

---
+++ PDF_QUESTIONS.CPP/Q24.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+
+int main()
+{
+    int i = 1;
+    int n;
+    cout<<"Enter a number: ";
+    cin >> n;
+    while(i <= n)
+    {
+    cout << "kkkk" << endl;
+    i++;
+    }
+    return 0;
+}
