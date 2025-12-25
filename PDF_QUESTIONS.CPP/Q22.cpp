db8ab110f607fc78f2a7ba9c0338eb1a8643d316
Pooja Sahu <misspooja.4816@gmail.com>
2025-12-25 16:00:18 +0530

    sheet -> Q 22

---
+++ PDF_QUESTIONS.CPP/Q22.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int age;
+    cout<<"Enter your current age: ";
+    cin>>age;
+    if(age>18)
+    cout<<"you can vote";
+    else
+    cout <<"No, you can't vote";
+}
