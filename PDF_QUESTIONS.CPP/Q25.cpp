cb27ef29e70a9f49e3acaded17a3c840752ff654
Pooja Sahu <misspooja.4816@gmail.com>
2025-12-26 17:43:13 +0530

    sheet -> Q 25

---
+++ PDF_QUESTIONS.CPP/Q25.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+
+int main()
+{
+    int i = 0;
+    //int sum = 0;
+    while(i <= 10)
+    {
+        cout << i*15  << endl;
+        //sum = (i*5) + 15 ;
+        i++;
+    }
+}
