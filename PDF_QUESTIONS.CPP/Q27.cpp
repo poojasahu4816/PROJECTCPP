3eb1e5103aed252ea13e227ee2a210f7edb18350
Pooja Sahu <misspooja.4816@gmail.com>
2025-12-26 17:44:34 +0530

    sheet -> Q 27

---
+++ PDF_QUESTIONS.CPP/Q27.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+
+int main()  //even number
+{
+    int i = 0;
+    while(i <= 10)
+    {
+    cout << i * 2 << endl;
+    i++;
+     }
+}
