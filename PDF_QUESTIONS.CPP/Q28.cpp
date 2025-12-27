ef8e195ea21e104257cdc44cc56f0d12881760d0
Pooja Sahu <misspooja.4816@gmail.com>
2025-12-27 20:36:40 +0530

    sheet -> 28

---
+++ PDF_QUESTIONS.CPP/Q28.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+
+int main()   // odd number
+{
+    int i = 1;
+    while(i <= 10)
+    {
+        cout << (i*2)-1 << endl;
+        i++;
+    }
+}
