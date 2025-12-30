e38e802d80cb46adc6ebbcbf16c3ffa6d1ace0de
Pooja Sahu <misspooja.4816@gmail.com>
2025-12-30 17:48:00 +0530

    sheet -> Q. 48 b

---
+++ PDF_QUESTIONS.CPP/Q48b.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+
+int main()
+{
+    int i;
+    for(i=0; i<=5; i++){
+     for (int j=1; j<=3; j++){
+    
+        cout<<"*";
+     }
+    cout << "  " << endl;
+}
+}
